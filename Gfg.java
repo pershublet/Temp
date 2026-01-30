98de2a7076f98cece4dbe65474787140376cbf50
pershublet <pershublet@outlook.com>
2026-01-30 11:48:03 +0000

    Update Gfg.java

--- Gfg.java
+++ Gfg.java
@@ -1,20 +1,15 @@
 class Solution {
-    public boolean isWordExist(char[][] mat, String word) {
-        final int n = mat.length;
-        final int m = mat[0].length;
-        final int w = word.length();
-        final HashMap<Character, Integer> targetMap = new HashMap<>();
-        Arrays.stream(word.toCharArray())
-            .forEach(c -> targetMap.put(c, targeting.getOrDefault(, 0) + 1);
-        final HashSet<Integer> hashSet = new HashSet<>();
+    public void rearrangeQueue(Queue<Integer> q) {
+        final ArrayDeque<Integer> p = new ArrayDeque<>();
+        final int n = q.size() / 2;
         
-        for (int x = 1; x <= w; x ++) {
-            if (w % x != 0) {
-                continue;
-            }
-
-            final int y = w / x;
+        for (int i = 0; i < n; i++) {
+            p.offerLast(q.poll());
         }
         
+        for (int i = 0; i < n; i++) {
+            q.offer(p.poll());
+            q.offer(q.poll());
+        }
     }
 }
